717ae47ae8c8b548c8d67f5e7da7a687e154c22b
Burak <200704002@st.maltepe.edu.tr>
2024-09-01 23:18:14 +0300

    Remove ignored files from repository

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -6,19 +6,123 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundView(topColor: .blue , bottomColor: Color("lightBlue"))
+            
+            VStack{
+                CityTextView(cityName: "Miami, FL")
+                
+                MainWeatherStatusView(imageName:"cloud.sun.fill", temperature: 76)
+                
+                VStack (spacing : 10){
+                   
+                    HStack (spacing: 20){
+                        WeatherDayView(dayOfWeek: "TUE", imageName: "sun.max.fill", temperature: 23)
+                        WeatherDayView(dayOfWeek: "WED", imageName: "cloud.sun.rain.fill", temperature: 21)
+                        WeatherDayView(dayOfWeek: "THU", imageName: "cloud.sun.fill", temperature: 22)
+                        WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.sun.fill", temperature: 23)
+                        WeatherDayView(dayOfWeek: "SAT", imageName: "sun.rain.fill", temperature: 21)
+                    }
+                    
+                    Spacer()
+                
+                    
+                    Button{
+                         print("tapped")
+                    } label : {
+                        Text("Change Day Time")
+                            .frame(width: 280, height: 50)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                }
+                
+            }
+            
+            
+            
         }
-        .padding()
+        
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek : String
+    var imageName : String
+    var temperature : Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundStyle(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(Color(.white))
+            
+        }
+        
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor : Color
+    var bottomColor : Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView : View {
+        
+    var cityName : String
+    
+    var body: some View{
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundStyle(Color(.white))
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView : View {
+    
+    var imageName : String
+    var temperature : Int
+    
+    var body: some View {
+        Image(systemName: imageName)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+        
+        Text("\(temperature)°")
+            .font(.system(size: 70, weight: .medium))
+            .foregroundStyle(.white)
+        
+            .padding(.bottom, 48)
+    }
 }
